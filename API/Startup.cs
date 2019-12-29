91ee10ad261f2cd10d66740b98857c3f59329d06
Boris Korobeinikov <slon2k@gmail.com>
2019-12-29 16:21:46 +0600

    Server validation

--- API/Startup.cs
+++ API/Startup.cs
@@ -14,6 +14,8 @@ using Microsoft.Extensions.Logging;
 using Persistence;
 using MediatR;
 using Application.Activities;
+using FluentValidation.AspNetCore;
+using API.Middleware;
 
 namespace API
 {
@@ -39,15 +41,20 @@ namespace API
                         .AllowAnyMethod()
                         .WithOrigins("http://localhost:3000")));
             services.AddMediatR(typeof(List.Handler).Assembly);
-            services.AddControllers();
+            services.AddControllers()
+                .AddFluentValidation(cfg => { 
+                    cfg.RegisterValidatorsFromAssemblyContaining<Create>();
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ErrorHandlingMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+               // app.UseDeveloperExceptionPage();
             }
 
             app.UseCors("CorsPolicy");
--- Application/Activities/Delete.cs
+++ Application/Activities/Delete.cs
@@ -3,6 +3,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Persistence;
+using System.Net;
+using Application.Errors;
 
 namespace Application.Activities
 {
@@ -28,7 +30,7 @@ namespace Application.Activities
 
                 if (activity == null)
                 {
-                    throw new Exception("Activity not found");
+                    throw new RestException(HttpStatusCode.NotFound, new {activity = "Not found"} );
                 }
 
                 _context.Remove(activity);
