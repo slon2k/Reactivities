571e9fbfac437bf1c2d228bab9dd9602ad6017a6
Boris Korobeinikov <slon2k@gmail.com>
2019-12-30 13:18:37 +0600

    Form changed to FinalForm

--- client-app/src/components/activity-form/ActivityForm.tsx
+++ client-app/src/components/activity-form/ActivityForm.tsx
@@ -1,10 +1,13 @@
-import React, { useState, FormEvent, useContext } from "react";
+import React, { useContext } from "react";
 import { Segment, Form, Button, Grid } from "semantic-ui-react";
 import { IActivity } from "../../models/activity";
 import { v4 as uuid } from "uuid";
 import { ActivityStore } from "../../store";
 import { observer } from "mobx-react-lite";
 import { Link, RouteComponentProps, withRouter } from "react-router-dom";
+import { Form as FinalForm, Field } from "react-final-form";
+import { TextInput, TextArea, SelectInput } from "../form";
+import { category } from "../../settings/categoryOptions";
 
 interface IProps {
   activity?: IActivity;
@@ -27,85 +30,90 @@ const ActivityForm: React.FC<IProps & RouteComponentProps> = ({
     city: ""
   };
 
-  const [form, setForm] = useState<IActivity>(activity || initialActivity);
+  const form = activity || initialActivity;
 
-  const handleChange = (
-    event: FormEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = event.currentTarget;
-    setForm({ ...form, [name]: value });
-  };
+  // const handleSubmit = async () => {
+  //   if (form.id !== "") {
+  //     console.log("Updating ...", form);
+  //     await updateActivity(form);
+  //     history.push(`/activities/${form.id}`);
+  //   } else {
+  //     form.id = uuid();
+  //     console.log("Creating ...", form);
+  //     await createActivity(form);
+  //     history.push(`/activities/${form.id}`);
+  //   }
+  // };
 
-  const handleSubmit = async () => {
-    if (form.id !== "") {
-      console.log("Updating ...", form);
-      await updateActivity(form);
-      history.push(`/activities/${form.id}`);
-    } else {
-      form.id = uuid();
-      console.log("Creating ...", form);
-      await createActivity(form);
-      history.push(`/activities/${form.id}`);
-    }
+  const handleFormSubmit = (values: any) => {
+    console.log(values);
   };
 
   return (
     <Grid>
       <Grid.Column width={10}>
         <Segment clearing>
-          <Form onSubmit={handleSubmit}>
-            <Form.Input
-              placeholder="Title"
-              name="title"
-              value={form.title}
-              onChange={handleChange}
-            />
-            <Form.TextArea
-              rows={2}
-              placeholder="Description"
-              name="description"
-              value={form.description}
-              onChange={handleChange}
-            />
-            <Form.Input
-              placeholder="Category"
-              name="category"
-              value={form.category}
-              onChange={handleChange}
-            />
-            <Form.Input
-              type="datetime-local"
-              name="date"
-              value={form.date}
-              onChange={handleChange}
-            />
-            <Form.Input
-              placeholder="City"
-              name="city"
-              value={form.city}
-              onChange={handleChange}
-            />
-            <Form.Input
-              placeholder="Venue"
-              name="venue"
-              value={form.venue}
-              onChange={handleChange}
-            />
-            <Button
-              floated="right"
-              positive
-              type="submit"
-              loading={submitting}
-              content="Submit"
-            />
-            <Button
-              as={Link}
-              to={`/activities/${form.id}`}
-              floated="right"
-              type="button"
-              content="Cancel"
-            />
-          </Form>
+          <FinalForm
+            onSubmit={handleFormSubmit}
+            render={({ handleSubmit }) => {
+              return (
+                <Form onSubmit={handleSubmit}>
+                  <Field
+                    placeholder="Title"
+                    name="title"
+                    value={form.title}
+                    component={TextInput}
+                  />
+                  <Field
+                    rows={2}
+                    placeholder="Description"
+                    name="description"
+                    value={form.description}
+                    component={TextArea}
+                  />
+                  <Field
+                    placeholder="Category"
+                    name="category"
+                    options={category}
+                    value={form.category}
+                    component={SelectInput}
+                  />
+                  <Field
+                    type="datetime-local"
+                    name="date"
+                    value={form.date}
+                    component={TextInput}
+                  />
+                  <Field
+                    placeholder="City"
+                    name="city"
+                    value={form.city}
+                    component={TextInput}
+                  />
+                  <Field
+                    placeholder="Venue"
+                    name="venue"
+                    value={form.venue}
+                    component={TextInput}
+                  />
+                  <Button
+                    floated="right"
+                    positive
+                    type="submit"
+                    loading={submitting}
+                    content="Submit"
+                  />
+                  <Button
+                    as={Link}
+                    to={`/activities/${form.id}`}
+                    floated="right"
+                    type="button"
+                    content="Cancel"
+                  />
+                </Form>
+              );
+            }}
+          />
         </Segment>
       </Grid.Column>
     </Grid>
---
+++ client-app/src/components/form/index.ts
@@ -0,0 +1,5 @@
+import TextInput from "./TextInput";
+import TextArea from "./TextArea";
+import SelectInput from "./SelectInput"
+
+export {TextInput, TextArea, SelectInput}
