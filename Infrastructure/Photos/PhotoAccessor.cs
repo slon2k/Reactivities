441c9cd53afc34bd4819b62640b41ff16b953e55
Boris Korobeinikov <slon2k@gmail.com>
2020-01-06 10:21:19 +0600

    Cloudinary upload interface implemented

--- Infrastructure/Photos/PhotoAccessor.cs
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -1,13 +1,46 @@
 using Application.Interfaces;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using Infrastructure.Photos;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
 
 namespace Application.Photos
 {
     public class PhotoAccessor : IPhotoAccessor
     {
+        private readonly Cloudinary _cloudinary;
+        public PhotoAccessor(IOptions<CloudinarySettings> config)
+        {
+            var account = new Account(
+                config.Value.CloudName,
+                config.Value.ApiKey,
+                config.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(account);
+        }
+
         public PhotoUploadResult AddPhoto(IFormFile file)
         {
-            throw new System.NotImplementedException();
+            var uploadResult = new ImageUploadResult();
+            
+            if (file.Length > 0)
+            {
+                using(var stream = file.OpenReadStream())
+                {
+                    uploadResult = _cloudinary.Upload(new ImageUploadParams 
+                    {
+                        File = new FileDescription(file.FileName, stream)
+                    });
+                }
+            }
+
+            return new PhotoUploadResult
+            {
+                PublicId = uploadResult.PublicId,
+                Url = uploadResult.SecureUri.AbsoluteUri
+            };
         }
 
         public string DeletePhoto(string publicId)
