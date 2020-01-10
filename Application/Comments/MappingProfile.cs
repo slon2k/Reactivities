7836baa2b45e6ab96168a103c974e8660611b023
Boris Korobeinikov <slon2k@gmail.com>
2020-01-10 13:43:39 +0600

    Comment Create Command created

---
+++ Application/Comments/MappingProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Domain;
+
+namespace Application.Comments
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Comment, CommentDto>()
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.Author.DisplayName))
+                .ForMember(d => d.UserName, o => o.MapFrom(s => s.Author.UserName))
+                .ForMember(d => d.Image, o => o.MapFrom(s => s.Author.Image.Url));
+        }
+    }
+}
