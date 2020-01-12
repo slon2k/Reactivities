d2d42de146cd043cf9f4eda4e243bcb2586d1f21
Boris Korobeinikov <slon2k@gmail.com>
2020-01-12 21:33:33 +0600

    APi get Followers List

--- API/Controllers/FollowersController.cs
+++ API/Controllers/FollowersController.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application.Followers;
+using Application.Profiles;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,6 +27,12 @@ namespace API.Controllers
         public async Task<ActionResult<Unit>> Unfollow(string username)
         {
             return await _mediator.Send(new Delete.Command{UserName = username});            
-        }       
+        }
+
+        [HttpGet("{username}/follow")]
+        public async Task<ActionResult<List<Profile>>> GetFollowings(string username, string predicate)
+        {
+            return await _mediator.Send(new List.Query{UserName = username, Predicate = predicate});
+        }      
     }
 }
